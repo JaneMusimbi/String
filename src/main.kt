45318faf6c3a025c746c3d2d01e14eb6eeb4c9cc
JaneMusimbi <janemusimbi@gmail.com>
2022-03-01 22:58:33 -0500

    second commit

---
+++ src/main.kt
@@ -0,0 +1,47 @@
+fun main(){
+    var name = "AkiraChix"
+    var x = name[1].toString()+name[3]+name[4]
+    println(x)
+    var w="Hi,my name is Jane and I am 23year old"
+    println(w)
+
+
+
+    var myself="musimbi"
+        var o= myself
+        println(o.length)
+
+    var name1 = "John"
+    var name2 = "Jane"
+    if (name1==name2)
+        println("that's me")
+    else (
+            println("i don't know who that is"))
+
+}
+
+fun parameter(name:String,age:Int):String{
+    var x=name.toString() +age
+    return x
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
